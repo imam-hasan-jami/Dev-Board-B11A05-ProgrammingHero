54be2827f92293a1f09c09ad03121bd0b320cb11
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-03-02 03:33:06 +0600

    added the activity log and clear history functionality

--- script/main.js
+++ script/main.js
@@ -26,6 +26,21 @@ for (let i = 0; i < completedBtn.length; i++) {
             assignedTask.innerText = convertedAssignedTask;
             completedTask.innerText = convertedCompletedTask;
 
+            let now = new Date();
+            let hours = now.getHours();
+            let minutes = now.getMinutes();
+            let seconds = now.getSeconds();
+
+            let timeString = `${hours}:${minutes}:${seconds}`;
+
+            let taskContainer = this.closest(".task-container");
+            let taskTitle = taskContainer.getElementsByClassName("task-title")[0].innerText;
+
+            let activityLog = document.getElementById("activity-log");
+            let newActivity = document.createElement("div");
+            newActivity.innerHTML = `<div style="font-family: 'Poppins', sans-serif; background-color: #F4F7FF; margin: 10px; padding: 10px; border-radius:8px">You have completed the task: <span style="font-weight: bold;">${taskTitle}</span> at ${timeString}</div>`;
+            activityLog.appendChild(newActivity);
+
             alert("Board Updated Successfully");
 
             this.disabled = true;
@@ -35,3 +50,9 @@ for (let i = 0; i < completedBtn.length; i++) {
         }
     })
 }
+
+// clear history button
+document.getElementById("btn-clear").addEventListener("click", function() {
+    let activityLog = document.getElementById("activity-log");
+    activityLog.innerHTML = "";
+})
