38e504f9c97ddcc7b5b74e20a2bdd5a96b7cd874
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-03-02 01:09:01 +0600

    added color-picker and current-date functionality

---
+++ utilities/utilities.js
@@ -0,0 +1,9 @@
+// Function to get a random color
+function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
