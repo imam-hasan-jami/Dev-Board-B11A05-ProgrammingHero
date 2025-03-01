3e7d1eb2d6711de3126939b897babf9dfb18a3a0
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-03-02 01:47:43 +0600

    added displayCurrentDate function

--- script/main.js
+++ script/main.js
@@ -1,17 +1,7 @@
 // showing the current date
 document.addEventListener("DOMContentLoaded", function () {
-    let now = new Date();
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-    let dayName = days[now.getDay()];
-    let monthName = months[now.getMonth()];
-    let date = now.getDate();
-    let year = now.getFullYear();
-
-    let dateDisplay = document.getElementById("dateDisplay");
-    dateDisplay.innerHTML = `${dayName} , <br><span class="font-bold">${monthName} ${date}, ${year}</span>`;
-});
+    displayCurrentDate();
+})
 
 // pick a random color
 document.getElementById("random-color").addEventListener("click", function () {
--- utilities/utilities.js
+++ utilities/utilities.js
@@ -1,3 +1,18 @@
+// Function to display current date
+function displayCurrentDate() {
+    let now = new Date();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    let dayName = days[now.getDay()];
+    let monthName = months[now.getMonth()];
+    let date = now.getDate();
+    let year = now.getFullYear();
+
+    let dateDisplay = document.getElementById("date-display");
+    dateDisplay.innerHTML = `${dayName} , <br><span class="font-bold">${monthName} ${date}, ${year}</span>`;
+}
+
 // Function to get a random color
 function getRandomColor() {
     let letters = "0123456789ABCDEF";
