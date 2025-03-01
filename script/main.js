43dd7530b3a44a1939abcfc1d2e89861373d3c02
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-03-02 02:42:20 +0600

    added plus minus functionality when the button is pressed

--- script/main.js
+++ script/main.js
@@ -8,3 +8,30 @@ document.getElementById("random-color").addEventListener("click", function () {
     let randomColor = getRandomColor();
     document.body.style.backgroundColor = randomColor;
 })
+
+// task completed button
+completedBtn = document.getElementsByClassName("btn-completed");
+for (let i = 0; i < completedBtn.length; i++) {
+    completedBtn[i].addEventListener("click", function () {
+        let assignedTask = document.getElementById("assigned-task");
+        let completedTask = document.getElementById("completed-task");
+
+        let convertedAssignedTask = parseInt(assignedTask.innerText);
+        let convertedCompletedTask = parseInt(completedTask.innerText);
+
+        if (convertedAssignedTask > 0) {
+            convertedAssignedTask--;
+            convertedCompletedTask++;
+
+            assignedTask.innerText = convertedAssignedTask;
+            completedTask.innerText = convertedCompletedTask;
+
+            alert("Board Updated Successfully");
+
+            this.disabled = true;
+
+            this.style.backgroundColor = "#949ee3";
+            this.style.cursor = "not-allowed";
+        }
+    })
+}
